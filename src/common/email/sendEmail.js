c388c1869425d1673e1f24865672a64f60a6447f
karee <kareem.gomaa.2001@gmail.com>
2026-04-06 13:13:20 +0200

    use resend for email

--- src/common/email/sendEmail.js
+++ src/common/email/sendEmail.js
@@ -1,24 +1,16 @@
-import nodemailer from "nodemailer";
+import { Resend } from 'resend';
 
-const sendEmail = async options => {
-    console.log("Configuring transport with:", process.env.MAILTRAP_USER);
-    const transporter = nodemailer.createTransport({
-        host: process.env.MAILTRAP_HOST,
-        port: Number(process.env.MAILTRAP_PORT),
-        secure: true,
-        auth: {
-            user: process.env.MAILTRAP_USER,
-            pass: process.env.MAILTRAP_PASS,
-        },
-    });
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendEmail = async (options) => {
     console.log("Trying to send email to:", options.email);
-    const mailOptions = {
-        from: process.env.MAILTRAP_USER,
+    await resend.emails.send({
+        from: 'onboarding@resend.dev',
         to: options.email,
         subject: options.subject,
         text: options.text,
-    };
-    await transporter.sendMail(mailOptions);
+    });
+    console.log("Email sent successfully");
 }
 
 export default sendEmail;
